a8224d2434562d72b2b4ad784fd1970eed7d8426
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2026-01-09 23:09:01 +0200

    feat : remove unused post, author and category schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,9 +1,5 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
-import { blockContentType } from './blockContentType'
-import { categoryType } from './categoryType'
-import { postType } from './postType'
-import { authorType } from './authorType'
 import { heroType } from './heroType'
 import { callCoverageType } from './callCoverageType'
 import { testimonialsType } from './testimonialsType'
@@ -13,10 +9,6 @@ import { onboardingType } from './onboardingType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    blockContentType,
-    categoryType,
-    postType,
-    authorType,
     heroType,
     callCoverageType,
     testimonialsType,
